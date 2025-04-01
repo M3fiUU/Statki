30e7370860657486cb1300d6ed449cfa45b330e9
maikelele <lahcim.md@gmail.com>
2025-04-01 21:42:08 +0200

    fixed ships placement

--- game_logic.py
+++ game_logic.py
@@ -5,12 +5,46 @@ def can_place_ship(board, x, y, size, direction):
         for i in range(size):
             if board[y][x + i] != "~":
                 return False
+        if y != 0:
+            for i in range(size):
+                if board[y - 1][x + i] != "~":
+                    return False
+        if y != 9: 
+            for i in range(size):
+                if board[y + 1][x + i] != "~":
+                    return False
+
+        if x != 0:
+            for i in range(size):
+                if board[y][x + i - 1] != "~":
+                    return False
+
+        if x + size != 10:
+            for i in range(size):
+                if board[y][x + i + 1] != "~":
+                    return False
     else:
         if y + size > 10:
             return False
         for i in range(size):
             if board[y + i][x] != "~":
                 return False
+        if x != 0:
+            for i in range(size):
+                if board[y + i][x - 1] != "~":
+                    return False
+        if x != 9:
+            for i in range(size):
+                if board[y + i][x + 1] != "~":
+                    return False
+        if y != 0:
+            for i in range(size):
+                if board[y - 1][x] != "~":
+                    return False
+        if y != 9:
+            for i in range(size):
+                if board[y + i + 1][x] != "~":
+                    return False
     return True
 
 def check_hit(board, x, y):
@@ -29,8 +63,8 @@ def all_ships_sunk(board):
 
 def is_ship_sunk(ships, board, x, y):
     for ship in ships:
-        if (x, y) in ship:
-            for sx, sy in ship:
+        if [y, x] in ship:
+            for sy, sx in ship:
                 if board[sy][sx] == "S":
                     return False
             return True
@@ -42,18 +76,10 @@ def create_empty_board():
 def place_ship(board, x, y, size, direction):
     coords = []
     if direction == "H":
-        if x + size > 10:
-            return None
         for i in range(size):
-            if board[y][x + i] != "~":
-                return None
             coords.append((y, x + i))
     else:  # direction == "V"
-        if y + size > 10:
-            return None
         for i in range(size):
-            if board[y + i][x] != "~":
-                return None
             coords.append((y + i, x))
 
     for cy, cx in coords:
